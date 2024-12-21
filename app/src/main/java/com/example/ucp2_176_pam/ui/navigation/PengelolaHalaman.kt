cb924df3d09ae1c35bbf74c3e6c0e599feba1ddc
riskalaluna <riskalaluna@gmail.com>
2024-12-21 23:15:49 +0700

    membuat fungsi PengelolaHalaman untuk mengatur dan mengelola navigasi

---
+++ app/src/main/java/com/example/ucp2_176_pam/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,14 @@
+package com.example.ucp2_176_pam.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+
+}
