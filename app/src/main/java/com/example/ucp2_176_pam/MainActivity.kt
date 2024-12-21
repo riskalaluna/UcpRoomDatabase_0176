3a1fb4e78f4d63e62374fdcd75bb9e1a6d540d2d
riskalaluna <riskalaluna@gmail.com>
2024-12-22 01:04:03 +0700

    menuliskan PengelolaHalaman di MainActivity agar halaman sesuai dengan yang di pengelolahalaman

--- app/src/main/java/com/example/ucp2_176_pam/MainActivity.kt
+++ app/src/main/java/com/example/ucp2_176_pam/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.ucp2_176_pam.ui.navigation.PengelolaHalaman
 import com.example.ucp2_176_pam.ui.theme.UCP2_176_PAMTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             UCP2_176_PAMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
