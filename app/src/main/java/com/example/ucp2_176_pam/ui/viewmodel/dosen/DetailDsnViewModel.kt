492b196da8f68d8da76ff8468c4a957da406fbee
riskalaluna <riskalaluna@gmail.com>
2024-12-22 00:17:05 +0700

    DetailDsnViewModel adalah ViewModel yang bertanggung jawab untuk mengelola data dan tampilan detail dosen

--- app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/dosen/DetailDsnViewModel.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/dosen/DetailDsnViewModel.kt
@@ -1,6 +1,56 @@
 package com.example.ucp2_176_pam.ui.viewmodel.dosen
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_176_pam.data.entity.Dosen
+import com.example.ucp2_176_pam.repository.RepositoryDsn
+import com.example.ucp2_176_pam.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DetailDsnViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDsn: RepositoryDsn,
+) : ViewModel() {
+    private val _nidn: String = checkNotNull(savedStateHandle[DestinasiDetail.NIDN])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryDsn.getDsn(_nidn)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+}
 
 data class DetailUiState(
     val detailUiEvent: DosenEvent = DosenEvent(),
