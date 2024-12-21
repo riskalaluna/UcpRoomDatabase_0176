ace8a31ac7cdabd4b88f336926f38c881e766f8a
riskalaluna <riskalaluna@gmail.com>
2024-12-22 00:13:27 +0700

    data class untuk menampung data yang akan ditampilkan di UI

--- app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/dosen/DetailDsnViewModel.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/dosen/DetailDsnViewModel.kt
@@ -2,6 +2,19 @@ package com.example.ucp2_176_pam.ui.viewmodel.dosen
 
 import com.example.ucp2_176_pam.data.entity.Dosen
 
+data class DetailUiState(
+    val detailUiEvent: DosenEvent = DosenEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == DosenEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != DosenEvent()
+}
+
 fun Dosen.toDetailUiEvent(): DosenEvent {
     return DosenEvent(
         nidn = nidn,
