f6a91022e8977fbec1a5568df686bb870fae77c9
riskalaluna <riskalaluna@gmail.com>
2024-12-22 00:20:06 +0700

    ComponentDetailDsn digunakan untuk menampilkan komponen detail informasi dosen

---
+++ app/src/main/java/com/example/ucp2_176_pam/ui/view/dosen/DetailDsnView.kt
@@ -0,0 +1,36 @@
+package com.example.ucp2_176_pam.ui.view.dosen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun ComponentDetailDsn (
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+) {
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
