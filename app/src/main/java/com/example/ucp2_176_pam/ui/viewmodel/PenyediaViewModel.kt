0452d059db90a0d2d95d4031766981746f9b77ee
riskalaluna <riskalaluna@gmail.com>
2024-12-22 01:02:47 +0700

    Melengkapi code di PenyediaViewModel

--- app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/PenyediaViewModel.kt
@@ -1,17 +1,33 @@
 package com.example.ucp2_176_pam.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ucp2_176_pam.DataDsnApp
+import com.example.ucp2_176_pam.ui.viewmodel.dosen.DetailDsnViewModel
 import com.example.ucp2_176_pam.ui.viewmodel.dosen.DosenViewModel
+import com.example.ucp2_176_pam.ui.viewmodel.dosen.HomeDsnViewModel
 
 object PenyediaViewModel {
 
     val Factory = viewModelFactory {
         initializer {
             DosenViewModel(
+                DataDsnApp().containerApp.repositoryDsn
+            )
+        }
+
+        initializer {
+            HomeDsnViewModel(
+                DataDsnApp().containerApp.repositoryDsn
+            )
+        }
+
+        initializer {
+            DetailDsnViewModel(
+                createSavedStateHandle(),
                 DataDsnApp().containerApp.repositoryDsn
             )
         }
