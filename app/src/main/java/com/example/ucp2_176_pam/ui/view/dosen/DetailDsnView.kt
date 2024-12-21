890037ca7b229a632eaf8c92d6561ab85d250f98
riskalaluna <riskalaluna@gmail.com>
2024-12-22 01:02:18 +0700

    menyelesaikan code di DetailDsnView

--- app/src/main/java/com/example/ucp2_176_pam/ui/view/dosen/DetailDsnView.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/view/dosen/DetailDsnView.kt
@@ -1,14 +1,21 @@
 package com.example.ucp2_176_pam.ui.view.dosen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,6 +23,80 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2_176_pam.data.entity.Dosen
+import com.example.ucp2_176_pam.ui.viewmodel.dosen.DetailDsnViewModel
+import com.example.ucp2_176_pam.ui.viewmodel.dosen.DetailUiState
+import com.example.ucp2_176_pam.ui.viewmodel.dosen.toDosenEntity
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2_176_pam.ui.custumwidget.CstTopAppBar
+import com.example.ucp2_176_pam.ui.viewmodel.PenyediaViewModel
+
+@Composable
+fun DetailDsnView (
+    modifier: Modifier = Modifier,
+    viewModel: DetailDsnViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { }
+) {
+    Scaffold (
+        topBar = {
+            CstTopAppBar(
+                judul = "Detail Dosen",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        }
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+
+        BodyDetailDsn(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState
+        )
+    }
+}
+
+
+
+@Composable
+fun BodyDetailDsn(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState()
+) {
+    when {
+        detailUiState.isLoading -> {
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator() //Tampilkan loading
+            }
+        }
+        detailUiState.isUiEventNotEmpty -> {
+            Column (
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailDsn(
+                    dosen = detailUiState.detailUiEvent.toDosenEntity(),
+                    modifier = Modifier
+                )
+            }
+        }
+
+        detailUiState.isUiEventEmpty -> {
+            Box (
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
 
 
 @Composable
