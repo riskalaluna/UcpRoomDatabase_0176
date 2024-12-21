33e77cdfb60ca88748e7a6038220b48b4cb7d582
riskalaluna <riskalaluna@gmail.com>
2024-12-22 00:25:21 +0700

    menampilkan informasi detail dari objek Dosen dalam sebuah Card

--- app/src/main/java/com/example/ucp2_176_pam/ui/view/dosen/DetailDsnView.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/view/dosen/DetailDsnView.kt
@@ -1,15 +1,50 @@
 package com.example.ucp2_176_pam.ui.view.dosen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2_176_pam.data.entity.Dosen
 
+
+@Composable
+fun ItemDetailDsn (
+    modifier: Modifier = Modifier,
+    dosen: Dosen
+) {
+    Card (
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailDsn(judul = "NIDN", isinya = dosen.nidn)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailDsn(judul = "Nama", isinya = dosen.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailDsn(judul = "Jenis Kelamin", isinya = dosen.jenisKelamin)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
 
 @Composable
 fun ComponentDetailDsn (
