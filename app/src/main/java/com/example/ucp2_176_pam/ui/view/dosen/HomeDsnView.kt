c1ff83b9d49a2c58e423d56e222f5ea2ff8b97a8
riskalaluna <riskalaluna@gmail.com>
2024-12-21 23:39:36 +0700

    menyusun tampilan untuk Card dan mengatur tampilan pada bagian nama

--- app/src/main/java/com/example/ucp2_176_pam/ui/view/dosen/HomeDsnView.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/view/dosen/HomeDsnView.kt
@@ -1,8 +1,22 @@
 package com.example.ucp2_176_pam.ui.view.dosen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ucp2_176_pam.data.entity.Dosen
 
 @OptIn (ExperimentalMaterial3Api::class)
@@ -12,5 +26,28 @@ fun CardDsn(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = { }
 ) {
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "" )
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = dsn.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
 
+        }
+    }
 }
