f920fa140e2bc88e2dcc0f3ce047c7d4d0690331
riskalaluna <riskalaluna@gmail.com>
2024-12-21 22:50:13 +0700

    menggunakan Box sebagai layout utama dan menambahkan tombol "Kembali"

--- app/src/main/java/com/example/ucp2_176_pam/ui/custumwidget/CstTopAppBar.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/custumwidget/CstTopAppBar.kt
@@ -1,8 +1,19 @@
 package com.example.ucp2_176_pam.ui.custumwidget
 
 import android.widget.Button
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun CstTopAppBar(
@@ -11,5 +22,32 @@ fun CstTopAppBar(
     judul: String,
     modifier: Modifier = Modifier
 ) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        if (showBackButton) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextButton(
+                    onClick = onBack,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                ) {
+                    Text("Kembali")
+                }
+                Spacer(modifier = Modifier.weight(2f))
+            }
+        }
 
+        Text(
+            text = judul,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
 }
