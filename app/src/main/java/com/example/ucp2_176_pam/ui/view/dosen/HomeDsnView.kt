c96c97a2498d46ef3cef94b978aea9bea97baa8b
riskalaluna <riskalaluna@gmail.com>
2024-12-21 23:31:51 +0700

    fungsi CardDsn untuk menampilkan data dosen dalam bentuk Card

---
+++ app/src/main/java/com/example/ucp2_176_pam/ui/view/dosen/HomeDsnView.kt
@@ -0,0 +1,16 @@
+package com.example.ucp2_176_pam.ui.view.dosen
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.ucp2_176_pam.data.entity.Dosen
+
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
+fun CardDsn(
+    dsn: Dosen,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+
+}
