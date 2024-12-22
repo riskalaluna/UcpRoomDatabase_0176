e015e1dd6db1bd3c17b4de336871e92c415164e2
riskalaluna <riskalaluna@gmail.com>
2024-12-22 15:32:28 +0700

    membuat data class untuk yang digunakan untuk merepresentasikan status UI (User Interface)

--- app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -2,6 +2,25 @@ package com.example.ucp2_176_pam.ui.viewmodel.matakuliah
 
 import com.example.ucp2_176_pam.data.entity.MataKuliah
 
+data class MKUIState(
+    val matakuliahEvent: MataKuliahEvent = MataKuliahEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null,
+)
+
+data class FormErrorState(
+    val kode: String? = null,
+    val nama: String? = null,
+    val sks: String? = null,
+    val jenis: String? = null,
+    val dosenPengampu: String? = null
+) {
+    fun isValid(): Boolean {
+        return kode == null && nama == null && sks == null &&
+                jenis == null && dosenPengampu == null
+    }
+}
+
 data class MataKuliahEvent (
     val kode: String = "",
     val nama: String = "",
