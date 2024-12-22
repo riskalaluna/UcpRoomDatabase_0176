fa4b0eb8a346b14c58d24767fa7127c5e1336e82
riskalaluna <riskalaluna@gmail.com>
2024-12-23 01:47:36 +0700

    file yang berisikan code untuk menambahkan data dosen

--- app/src/main/java/com/example/ucp2_176_pam/ui/view/dosen/InsertDsnView.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/view/dosen/InsertDsnView.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2_176_pam.data.entity.Dosen
