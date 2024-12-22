f83cd60785a253eda86f809ac99dc9d6f868f401
riskalaluna <riskalaluna@gmail.com>
2024-12-22 23:50:18 +0700

    membuat Container yang mengelola akses ke repositoryDsn dan repositoryMK

--- app/src/main/java/com/example/ucp2_176_pam/dependenciesinjection/InterfaceContainerApp.kt
+++ app/src/main/java/com/example/ucp2_176_pam/dependenciesinjection/InterfaceContainerApp.kt
@@ -1,16 +1,24 @@
 package com.example.ucp2_176_pam.dependenciesinjection
 
 import android.content.Context
-import com.example.ucp2_176_pam.data.database.DataDsnDatabase
+import com.example.ucp2_176_pam.data.database.AppDatabase
 import com.example.ucp2_176_pam.repository.LocalRepositoryDsn
+import com.example.ucp2_176_pam.repository.LocalRepositoryMK
 import com.example.ucp2_176_pam.repository.RepositoryDsn
+import com.example.ucp2_176_pam.repository.RepositoryMK
 
 interface InterfaceContainerApp {
     val repositoryDsn: RepositoryDsn
+    val repositoryMK: RepositoryMK
 }
-
 class ContainerApp(private val context: Context) : InterfaceContainerApp {
     override val repositoryDsn: RepositoryDsn by lazy {
-        LocalRepositoryDsn(DataDsnDatabase.getDatabase(context).dosenDao())
+        LocalRepositoryDsn(AppDatabase.getDatabase(context).dosenDao())
+    }
+
+    override val repositoryMK: RepositoryMK by lazy {
+        LocalRepositoryMK(AppDatabase.getDatabase(context).mataKuliahDao())
     }
 }
+
+
