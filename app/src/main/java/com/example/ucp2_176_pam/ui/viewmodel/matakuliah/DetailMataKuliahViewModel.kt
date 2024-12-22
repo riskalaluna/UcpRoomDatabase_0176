f5b64e0c15aebb0dc1877ae7270879ac72df249e
riskalaluna <riskalaluna@gmail.com>
2024-12-23 00:49:55 +0700

    data class DetailUiState untuk menyimpan status UI dalam konteks detail mata kuliah

--- app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/matakuliah/DetailMataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/matakuliah/DetailMataKuliahViewModel.kt
@@ -2,6 +2,18 @@ package com.example.ucp2_176_pam.ui.viewmodel.matakuliah
 
 import com.example.ucp2_176_pam.data.entity.MataKuliah
 
+data class DetailUiState(
+    val detailUiEvent: MataKuliahEvent = MataKuliahEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MataKuliahEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MataKuliahEvent()
+}
 
 fun MataKuliah.toDetailUiEvent(): MataKuliahEvent {
     return MataKuliahEvent(
