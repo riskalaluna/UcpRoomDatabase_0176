8ff05701b8e5373377839fce7f829741ab0a6057
riskalaluna <riskalaluna@gmail.com>
2024-12-23 00:24:38 +0700

    HomeMataKuliahView yang mengelola data terkait Mata Kuliah dan menyediakan status UI

---
+++ app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/matakuliah/HomeMataKuliahViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.ucp2_176_pam.ui.viewmodel.matakuliah
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2_176_pam.data.entity.MataKuliah
+import com.example.ucp2_176_pam.repository.RepositoryMK
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeMataKuliahViewModel (
+    private val repositoryMK: RepositoryMK
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMK.getAllMK()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMK = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
+
+data class HomeUiState(
+    val listMK: List<MataKuliah> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
