bc4876f023ffc3d2bf6846913c63790252504d83
riskalaluna <riskalaluna@gmail.com>
2024-12-23 00:53:53 +0700

    bertanggung jawab untuk mengelola status detail mata kuliah

--- app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/matakuliah/DetailMataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/viewmodel/matakuliah/DetailMataKuliahViewModel.kt
@@ -1,6 +1,64 @@
 package com.example.ucp2_176_pam.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_176_pam.data.entity.MataKuliah
+import com.example.ucp2_176_pam.repository.RepositoryMK
+import com.example.ucp2_176_pam.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DetailMataKuliahViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMK: RepositoryMK,
+) : ViewModel() {
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiDetail.KODE])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMK.getMK(_kode)
+        .filterNotNull()
+        .map{
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+
+    fun deleteMK() {
+        detailUiState.value.detailUiEvent.toMataKuliahEntity().let{
+            viewModelScope.launch {
+                repositoryMK.deleteMK(it)
+            }
+        }
+    }
+}
 
 data class DetailUiState(
     val detailUiEvent: MataKuliahEvent = MataKuliahEvent(),
