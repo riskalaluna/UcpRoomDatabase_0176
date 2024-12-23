460e445908f3158a5341874d137d304d9689590f
riskalaluna <riskalaluna@gmail.com>
2024-12-23 14:58:33 +0700

    memperbaiki typo dan menyesuaikan code

--- app/src/main/java/com/example/ucp2_176_pam/ui/view/matakuliah/DetailMataKuliahView.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/view/matakuliah/DetailMataKuliahView.kt
@@ -1,23 +1,152 @@
 package com.example.ucp2_176_pam.ui.view.matakuliah
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2_176_pam.data.entity.MataKuliah
+import com.example.ucp2_176_pam.ui.custumwidget.CstTopAppBar
+import com.example.ucp2_176_pam.ui.viewmodel.PenyediaViewModel
+import com.example.ucp2_176_pam.ui.viewmodel.matakuliah.DetailMataKuliahViewModel
+import com.example.ucp2_176_pam.ui.viewmodel.matakuliah.DetailUiState
+import com.example.ucp2_176_pam.ui.viewmodel.matakuliah.toMataKuliahEntity
+
+@Composable
+fun DetailMataKuliahView (
+    modifier: Modifier = Modifier,
+    viewModel: DetailMataKuliahViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+) {
+    Scaffold (
+        topBar = {
+            CstTopAppBar(
+                judul = "Detail Mata Kuliah",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailUiState.value.detailUiEvent.kode) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa",
+                )
+            }
+        }
+
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+
+        BodyDetailMK(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deleteMK()
+                onDeleteClick()
+            }
+        )
+    }
+}
+
+@Composable
+fun BodyDetailMK(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = { }
+) {
+    var deleteConfirmationRequired by rememberSaveable {  mutableStateOf(false) }
+    when {
+        detailUiState.isLoading -> {
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator() //Tampilkan loading
+            }
+        }
+        detailUiState.isUiEventNotEmpty -> {
+            Column (
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailMK(
+                    mataKuliah = detailUiState.detailUiEvent.toMataKuliahEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancle = {
+                            deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailUiState.isUiEventEmpty -> {
+            Box (
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun ItemDetailMK (
@@ -50,7 +179,7 @@ fun ItemDetailMK (
             ComponentDetailMK(judul = "jenis", isinya = mataKuliah.jenis )
             Spacer(modifier = Modifier.padding(4.dp))
 
-            ComponentDetailMK(judul = "Angkatan", isinya = mataKuliah.dosenPengampu )
+            ComponentDetailMK(judul = "Dosen Pengampu", isinya = mataKuliah.dosenPengampu )
             Spacer(modifier = Modifier.padding(4.dp))
         }
     }
