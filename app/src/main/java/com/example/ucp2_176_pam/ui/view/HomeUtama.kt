b8b4e548962fee9d50b990c124a607486503f73a
riskalaluna <riskalaluna@gmail.com>
2024-12-23 17:01:37 +0700

    menambahkan code untuk mempercantik tampilan di home utama

--- app/src/main/java/com/example/ucp2_176_pam/ui/view/HomeUtama.kt
+++ app/src/main/java/com/example/ucp2_176_pam/ui/view/HomeUtama.kt
@@ -3,19 +3,25 @@ package com.example.ucp2_176_pam.ui.view
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ucp2_176_pam.R
 import com.example.ucp2_176_pam.ui.navigation.AlamatNavigasi
 
@@ -28,32 +34,94 @@ fun HomeUtamaView(
     onDosen: () -> Unit
 
 ) {
-    Column( modifier = Modifier
-        .fillMaxSize()
-        .background(color = colorResource(id = R.color.white)),
-        verticalArrangement = Arrangement.Center,
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.purple_200
+                )
+            ),
         horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
-        Image(
-            painter = painterResource(R.drawable.logoumy),
-            contentDescription = null,
-            modifier = Modifier
-                .size(150.dp)
-        )
         Spacer(modifier = Modifier.padding(16.dp))
-        Button(
-            onClick = {
-                onMataKuliah() }
+
+        Row(
+            modifier = Modifier.padding(15.dp),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            Text(text = "MataKuliah")
+            Image(
+                painter = painterResource(
+                    id = R.drawable.logoumy
+                ),
+                contentDescription = " ",
+                modifier = Modifier.size(50.dp)
+            )
+
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Black,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Unggul dan Islami",
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
+                )
+            }
         }
-        Spacer(modifier = Modifier.padding(16.dp))
-        Button(
-            onClick = {
-                onDosen() }
+
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(
+                        topEnd = 15.dp,
+                        topStart = 15.dp
+                    )
+                )
+                .fillMaxSize(),
         ) {
-            Text(text = "Dosen")
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Teknologi Informasi",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 19.sp
+                )
+                Text(
+                    text = "Menjadi Program Studi Unggul dan Islami",
+                    fontWeight = FontWeight.Light
+                )
+                Spacer(
+                    modifier = Modifier
+                        .padding(16.dp)
+                )
+                Button(
+                    onClick = {
+                        onMataKuliah()
+                    }
+                ) {
+                    Text(text = "MataKuliah")
+                }
+                Spacer(modifier = Modifier.padding(16.dp))
+                Button(
+                    onClick = {
+                        onDosen()
+                    }
+                ) {
+                    Text(text = "Dosen")
+                }
+            }
         }
     }
-
 }
